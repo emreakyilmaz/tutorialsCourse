4f412832581cb5559900f09f525f12bbff51c47f
emreakyilmaz <emreakyilmaz@gmail.com>
2022-06-18 15:25:05 +0300

    Agent file is deleted
    No longer required

--- Gridworld_LazyProgrammer/main.py
+++ Gridworld_LazyProgrammer/main.py
@@ -1,5 +1,24 @@
 from Gridworld import Gridworld
 
+def print_values(V, g):
+  for i in range(g.rows):
+    print("---------------------------")
+    for j in range(g.cols):
+      v = V.get((i,j), 0)
+      if v >= 0:
+        print(" %.2f|" % v, end="")
+      else:
+        print("%.2f|" % v, end="") # -ve sign takes up an extra space
+    print("")
+
+def print_policy(P, g):
+  for i in range(g.rows):
+    print("---------------------------")
+    for j in range(g.cols):
+      a = P.get((i,j), ' ')
+      print("  %s  |" % a, end="")
+    print("")
+
 if  __name__ == '__main__':
     transition_probs = {}
     rewards = {}
@@ -15,4 +34,17 @@ if  __name__ == '__main__':
                     if s2 in grid.rewards:
                         rewards[s,a,s2] = grid.rewards[s2]
 
-    print(transition_probs)
+    ### fixed policy ###
+    policy = {
+        (2, 0): 'U',
+        (1, 0): 'U',
+        (0, 0): 'R',
+        (0, 1): 'R',
+        (0, 2): 'R',
+        (1, 2): 'U',
+        (2, 1): 'R',
+        (2, 2): 'U',
+        (2, 3): 'L',
+    }
+
+    print_policy(policy, grid)
